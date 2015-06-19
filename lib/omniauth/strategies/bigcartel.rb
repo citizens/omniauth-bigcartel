cd5c5ceba4b84c65ee1db7055c7ba86ae9c08817
Dylan Montgomery <mail@citizensinspace.com>
2015-06-19 00:42:11 -0400

    set bigcartel oauth paths

--- lib/omniauth/strategies/bigcartel.rb
+++ lib/omniauth/strategies/bigcartel.rb
@@ -5,7 +5,11 @@ module OmniAuth
     class Bigcartel < OmniAuth::Strategies::OAuth2
       option :name, :bigcartel
 
-      option :client_options, { site: 'https://my.bigcartel.com'}
+      option :client_options, { 
+        site: 'https://my.bigcartel.com',
+        authorize_url: 'https://my.bigcartel.com/oauth/authorize',
+        access_token_path: '/oauth/token',
+      }
 
       uid { request.params['account_id'] }
     end
